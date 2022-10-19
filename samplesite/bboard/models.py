046cac2ba4a587c1a01fec3a3bc7bad708b6a7fb
laughing <eugene3107@yandex.ru>
2022-10-19 17:02:34 +0500

    chapter_2 take urls to pages

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -15,8 +15,13 @@ class Bb(models.Model):
 class Rubric(models.Model):
     name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
 
         verbose_name_plural = 'Рубрики'
         verbose_name = 'Рубрика'
         ordering = ['name']
+
+
